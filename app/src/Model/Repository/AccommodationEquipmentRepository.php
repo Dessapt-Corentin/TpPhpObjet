7a7a1a02bff840f7e5a4f006b7f49d390ed8b03d
Dessapt-Corentin <corentin.dessapt-patin@lidem.education>
2024-12-15 22:08:41 +0100

    debut methode getbyaccommodationId

--- app/src/Model/Repository/AccommodationEquipmentRepository.php
+++ app/src/Model/Repository/AccommodationEquipmentRepository.php
@@ -17,10 +17,26 @@ class AccommodationEquipmentRepository extends Repository
         return $this->readAll(AccommodationEquipment::class);
     }
 
+    // Récupérer l'id de l'accommodations pour la mettre dans la table accommodations_equipments
     public function getById(int $id): ?AccommodationEquipment
     {
         return $this->readById(AccommodationEquipment::class, $id);
     }
+
+    // Récupérer les équipements d'un seul accommodation par son accommodation ID
+    public function getByAccommodationId(int $accommodation_id): array
+    {
+        $query = sprintf(
+            'SELECT * FROM `%s` WHERE accomodation_id=:accomodation_id',
+            $this->getTableName()
+        );
+
+        $sth = $this->pdo->prepare($query);
+        $sth->execute(['accomodation_id' => $accommodation_id]);
+        return $sth->fetchAll(\PDO::FETCH_CLASS, AccommodationEquipment::class);
+    }
+
+    
 
     public function create(array $accommodation_equipment): ?AccommodationEquipment
     {
